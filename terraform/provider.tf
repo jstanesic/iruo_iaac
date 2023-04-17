c43bcfd52d24d16671349cb125637b41efef2db6
Josip Stanesic <josip.stanei@gmail.com>
2023-04-17 16:43:09 +0200

    Removing pass

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "openstack" {
   user_name   = "admin"
   tenant_name = "admin"
-  password    = "***REMOVED***"
+  password    = "CHANGEME"
   auth_url    = "http://172.25.250.50:5000//v3"
 }
